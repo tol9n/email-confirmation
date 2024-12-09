4c162658da0bf6095fa65dc47c268c74fc43233c
tol9n <Shilkov109@gmail.com>
2024-12-09 22:42:19 +0500

    added web interface builded on fastapi

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import random
 import smtplib
+
+
+from fastapi import FastAPI
+
 import config
 
-def send_mail(mes_to:str, code:str):
+app = FastAPI()
+
+
+def send_mail(mes_to: str, code: any):
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.starttls()
     smtp.login(config.address, config.password)
-    smtp.sendmail(config.address,mes_to,code)
+    code = str(code)
+    smtp.sendmail(config.address, mes_to, code)
     smtp.quit()
 
+
+@app.get("/")
+def read_root(adr: str = 'default'):
+    code = random.randint(100000, 999999)
+    send_mail(adr, code)
+    return {"adrs" : adr, "code" : code}
